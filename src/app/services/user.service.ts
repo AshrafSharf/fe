b8cf0c155819ec856afc758a576b2600e1d034e8
Amit Kulkarni <pythoncpp@gmail.com>
2018-03-01 19:45:04 +0530

    added loading spinner

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,38 +5,38 @@ import { Router } from "@angular/router";
 import { Utils } from './../shared/utils';
 import { User } from '../shared/interfaces/user';
 import { Observable } from 'rxjs/Observable';
+import { LoaderService } from './loader.service';
 
 @Injectable()
 export class UserService implements CanActivate {
     
     constructor(
         private router: Router,
-        private http: Http) {}
+        private http: Http,
+        private loaderService:LoaderService) {}
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        console.log("canActiviate");
         if (sessionStorage.getItem('user_auth_status') == '1') {
             return true;
         }
-        console.log("canActiviate");
-        
         this.router.navigate(['login']);
         return false;
     }
 
     getUserByName(username:String) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeUser) + "/username/" + username;
-        
         let header = new Headers({'Content-Type':'application/json'});
-
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(result => {
+                    this.loaderService.hide();
                     return result.json();
                 });
     }
 
     authenticateUser(username:String, password:String) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeLogin);
         let body = { userName: username, password: password };
 
@@ -46,20 +46,24 @@ export class UserService implements CanActivate {
         return this.http
                 .post(url, body, requestOptions)
                 .map(result => {
+                    this.loaderService.hide();
                     sessionStorage["authorization_token"] = result.headers.get('authorization');
                     return { status: "OK"};
                 })
                 .catch((error: any) => {
+                    this.loaderService.hide();
                     return Observable.throw(error);
                 });
     }
 
     getOwners(callback: (users:User[])=> void) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeUser);
         this.http
             .get(url, Utils.getRequestOptions())
             .map(result => result.json())
             .subscribe(result => {
+                this.loaderService.hide();
                 callback(result.data as User[]);
             });
     }
--- src/app/services/variable.services.ts
+++ src/app/services/variable.services.ts
@@ -4,96 +4,126 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Utils } from '../shared/utils';
+import { LoaderService } from './loader.service';
 
 @Injectable()
 export class AppVariableService {
 
-    constructor(private http: Http) { }
+    constructor(
+        private http: Http,
+        private loaderService:LoaderService
+    ) { }
 
     public getBreakdownVariables(branchId, type) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable + "/findByVariableType/" + branchId + "/"  + type);
         return this.http
                 .get(url, Utils.getRequestOptions())
-                .map(result => result.json());
+                .map(result => {
+                    this.loaderService.hide();
+                    return result.json();
+                });
     }
 
     public createVariable(body) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable);
         console.log(body);
         return this.http
                 .post(url, body, Utils.getRequestOptions())
-                .map(result => result.json());
+                .map(result => {
+                    this.loaderService.hide();
+                    return result.json();
+                });
     }
 
     public updateVariable(body, id) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/" + id;
         console.log(body);
         return this.http
                 .put(url, body, Utils.getRequestOptions())
-                .map(result => result.json());
+                .map(result => {
+                    this.loaderService.hide();
+                    return result.json();
+                });
     }
 
     public getVariables(branchId) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/" + branchId;
         console.log(url);
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
+                    this.loaderService.hide();
                     return response.json()
                 });
     }
 
     public getVariableByName(branchId, varName){
-      let url = Utils.createUrl(Utils.routeVariable) + "/" + branchId+ "/"+varName;
-      return this.http
+        this.loaderService.show();
+        let url = Utils.createUrl(Utils.routeVariable) + "/" + branchId+ "/"+varName;
+        return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
-                  return response.json()
+                    this.loaderService.hide();
+                    return response.json()
                 });
     }
 
     public calculateVariableValues(id) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/docalc/" + id;
-
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
+                    this.loaderService.hide();
                     return response.json()
                 });
     }
 
     public extendValuesForMonths(id, index) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/doextendcalc/" + id + "/" + index;
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
+                    this.loaderService.hide();
                     return response.json()
                 });
     }
 
     public getCalculationsFor(id, startDate, endDate) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/doextendcalc/" + id + "/" + startDate + "/" + endDate;
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
+                    this.loaderService.hide();
                     return response.json()
                 });
     }
 
     public getVariablesForSuggestions(branchId) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/" + branchId + "/name";
-
         return this.http
                 .get(url, Utils.getRequestOptions())
                 .map(response => {
+                    this.loaderService.hide();
                     return response.json()
                 });
     }
 
     public deleteVariable(id) {
+        this.loaderService.show();
         let url = Utils.createUrl(Utils.routeVariable) + "/" + id;
         return this.http
                 .delete(url, Utils.getRequestOptions())
-                .map(result => result.json());
+                .map(result => {
+                    this.loaderService.hide();
+                    return result.json()
+                });
     }
 }

3bedcfa268a5dbc1f21facc8ffb8663c6edd06de
Amit Kulkarni <pythoncpp@gmail.com>
2018-03-13 16:15:01 +0530

    added few more features

--- src/app/dashboard/component-model/model.component.ts
+++ src/app/dashboard/component-model/model.component.ts
@@ -4,6 +4,8 @@ import 'rxjs/add/observable/of';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { StageComponent } from 'ng2-konva';
 import { Layer, Stage, Node, Shape, Rect, Transform, Circle, Star, RegularPolygon, Label, Tag, Text, Group } from 'konva';
+import { GenericMicroServiceTemplate } from './models/generic.micro.service.template';
+import { Template, TemplateEventsCallback, TemplateInterface } from './models/templates';
 
 @Component({
     selector: 'component-model',
@@ -11,12 +13,16 @@ import { Layer, Stage, Node, Shape, Rect, Transform, Circle, Star, RegularPolygo
     styleUrls: ['./model.component.css']
 })
 
-export class ComponentModelComponent implements OnInit {
+export class ComponentModelComponent implements OnInit, TemplateEventsCallback {
+
     @ViewChild(Layer) layer: Layer;
     @ViewChild(StageComponent) stage: StageComponent;
 
-    // list of drawing elements
-    public list: Array<any> = [];
+    // list of templatea
+    public templates: Array<Template> = new Array<Template>();
+
+    // selected template
+    public selectedTemplate:Template = null;
 
     // drawing area
     public width = 1000;
@@ -27,7 +33,15 @@ export class ComponentModelComponent implements OnInit {
     constructor() { }
 
     ngOnInit() { 
-        
+    }
+
+    private addMouseHandler() {
+        var st:Stage = this.stage.getStage();
+        var layer:Layer = st.getChildren()[0];
+       
+        st.on('click', (event) => {
+            this.selectedTemplate = null;
+        });
     }
 
     public configStage = Observable.of({
@@ -196,37 +210,12 @@ export class ComponentModelComponent implements OnInit {
     }
 
     public addGenericMicroService() {
-        var st:Stage = this.stage.getStage();
-        var layer:Layer = st.getChildren()[0];
+        let t = new GenericMicroServiceTemplate(this);
+        this.templates.push(t);
+        
+        this.addGroup(t.createUI());
 
-        let group = new Group({
-            x : Math.random() * 800,
-            y : Math.random() * 800,
-            draggable: true
-        })
-
-        let rect = new Rect({
-            x : 0,
-            y : 0,
-            width : 230,
-            height : 100,
-            fill : 'yellow',
-            stroke : 'black'
-        })
-
-        let label = new Text({
-            x : 10,
-            y : 10,
-            text:'Generic Micro Service template',
-            fontSize: this.fontSize,
-            fill: 'black'
-        });
-
-        group.add(rect);
-        group.add(label);
-        layer.add(group);
-
-        layer.draw();
+        this.addMouseHandler();
     }
 
     public addJavaMicroService() {
@@ -261,5 +250,86 @@ export class ComponentModelComponent implements OnInit {
         layer.add(group);
 
         layer.draw();
+    }
+
+    private generateUI() {
+        var st:Stage = this.stage.getStage();
+        var layer:Layer = st.getChildren()[0];
+
+        for (var index = 0; index < this.templates.length; index++) {
+            let template = this.templates[index];
+            layer.add(template.createUI());
+        }
+        layer.draw();
+
+    }
+
+    private addGroup(group) {
+        var st:Stage = this.stage.getStage();
+        var layer:Layer = st.getChildren()[0];
+
+        layer.add(group);
+        layer.draw();
+    }
+
+    public templateClicked(template: any) {
+        this.selectedTemplate = template.clone();
+    }
+
+    public addInterface() {
+        if (this.selectedTemplate != null) {
+            let intf = new TemplateInterface();
+            this.selectedTemplate.interfaces.push(intf);
+        }
+    }
+
+    public cancelPropertyChanges() {
+        this.selectedTemplate = null;
+    }
+
+    public savePropertyChanges() {
+        for (let index = 0; index < this.templates.length; index++) {
+            var template = this.templates[index];
+            if (template.identifier == this.selectedTemplate.identifier) {
+                // found the one to update
+
+                template.name = this.selectedTemplate.name;
+                template.interfaces = this.selectedTemplate.interfaces;
+
+                this.reloadTemplateUI(template);
+                break;
+            }
+        }
+    }
+
+    private reloadTemplateUI(template) {
+        var st:Stage = this.stage.getStage();
+        var layer:Layer = st.getChildren()[0];
+
+        template.uiGroup.remove();
+        layer.add(template.reloadUI());
+        layer.draw();
+    }
+
+    public deleteShape() {
+        if (this.selectedTemplate != null) {
+            for (let index = 0; index < this.templates.length; index++) {
+                var template = this.templates[index];
+                if (template.identifier == this.selectedTemplate.identifier) {
+                    // found the one to update
+                    template.uiGroup.remove();
+
+                    var st:Stage = this.stage.getStage();
+                    var layer:Layer = st.getChildren()[0];
+                    layer.draw();
+
+                    this.templates.splice(index, 1);     
+                    
+                    this.selectedTemplate = null;
+                    break;
+                }
+            }
+            
+        }
     }
 }
--- src/app/dashboard/component-model/models/templates.ts
+++ src/app/dashboard/component-model/models/templates.ts
@@ -1,3 +1,5 @@
+import { Guid } from '../../../shared/guid';
+import { Group } from 'konva';
 
 export interface TemplateInterfaceProperty {
     name:String;
@@ -10,18 +12,29 @@ export interface DownstreamInterface {
 }
 
 export class TemplateInterface {
+    public name:String = '';
+    public latency: String = '';
+    
     public properties: TemplateInterfaceProperty[] = new Array<TemplateInterfaceProperty>();
     public downstreamInterfaces: DownstreamInterface[] = new Array<DownstreamInterface>();
 }
 
-export class Template {
+export abstract class Template {
+    public identifier:String = '';
     public name:String = '';
     public interfaces:TemplateInterface[] = Array<TemplateInterface>();
+    protected callback: TemplateEventsCallback;
+    public uiGroup:Group = null;
+
+    constructor (callback) {
+        this.callback = callback;
+        this.identifier = Guid.newGuid().toString();
+    }
+
+    public abstract createUI(): any;
+    public abstract reloadUI(): any;
 }
 
-export class GenericMicroServiceTemplate extends Template {
-    
-    public createUI() {
-
-    }
+export interface TemplateEventsCallback {
+    templateClicked(template);
 }

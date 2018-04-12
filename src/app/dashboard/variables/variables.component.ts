cce60b0b1032acbe99820b642fba4398c43fdff5
godonnellwork1 <godonnellwork1@gmail.com>
2018-04-12 12:47:59 +0100

    Fixed issue 7228

--- src/app/dashboard/variables/variables.component.ts
+++ src/app/dashboard/variables/variables.component.ts
@@ -216,6 +216,7 @@ export class VariablesComponent implements OnInit {
                 this.subvariableList[this.editSubvariableIndex].probability = this.subvariablePercentage;
             }
             this.subvariableList[this.editSubvariableIndex].name = this.subvariableName;
+            this.subvariableListPercentage[this.editSubvariableIndex].name = this.subvariableName;
             var decimal = parseInt(this.subvariableValue)/100;
             this.subvariableList[this.editSubvariableIndex].value = decimal.toString();
             this.subvariableListPercentage[this.editSubvariableIndex].value = this.subvariableValue;
